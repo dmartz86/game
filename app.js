23fd4fa300ba5c02b0e8cd38087c1e252fa2d6ac
Daniel Martinez <dmartz86@gmail.com>
2015-01-09 15:13:21 -0500

    update app serices

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+var cfg = require('./config.json').port;
 var api = require('./api/routes').api;
 var web = require('./web/routes').web;
+var svr = require("http").Server;
+//var sio = require('socket.io')(cfg.rds.sio);
+var rds = require('socket.io-redis');
+var data = {host: 'localhost', port: cfg.rds};
 
-api.listen(require('./config.json').port.api, function() {
+api.listen(cfg.api, function() {
   console.log('app server up!');
 });
 
-web.listen(require('./config.json').port.web, function() {
+web.listen(cfg.web, function() {
   console.log('web server up!');
+  //sio.adapter(rds(data));
+  //console.log('sio server up!');
 });
-
