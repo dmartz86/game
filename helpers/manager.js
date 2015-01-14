7866993bc6e164daea5e7a3a87e537fe2625a6bc
Daniel Martinez <dmartz86@gmail.com>
2015-01-13 20:22:51 -0500

    Add 200 on update db

--- helpers/manager.js
+++ helpers/manager.js
@@ -2,6 +2,7 @@
 var response = function(req, res, err, rsp){
   if(err){ return res.json(500, {message: err}); }
   if(!rsp){ return res.send(404); }
+  if(rsp===1){ return res.send(200); }
   res.json(rsp);
 };
 
