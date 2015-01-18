3625a568cf2f8e22ccb5c0c033a778b387d1d189
Daniel Martinez <dmartz86@gmail.com>
2015-01-18 17:37:00 -0500

    Add mising routes and views.

--- web/routes.js
+++ web/routes.js
@@ -12,16 +12,32 @@ web.get('/', function(req, res){
   res.render('index/index');
 });
 
-web.get('/users/:id', function(req, res){
-  res.render('index/index',
-    {model: 'users', id: req.params.id}
-  );
-});
+//web.get('/users/:id', function(req, res){
+//  res.render('index/index',
+//    {model: 'users', id: req.params.id}
+//  );
+//});
+var resources = require('../config.json').resources;
+for(var r in resources){
 
-web.get('/roles/:id', function(req, res){
-  res.render('index/index',
-    {model: 'roles', id: req.params.id}
-  );
-});
+  web.get('/' + r , function(req, res){
+    res.render('index/index',
+      {model: r, id: req.params.id}
+    );
+  });
+
+  web.get('/' + r + '/:id', function(req, res){
+    res.render('index/index',
+      {model: r, id: req.params.id}
+    );
+  });
+
+  web.get('/' + r + '/new', function(req, res){
+    res.render('index/index',
+      {model: r, id: req.params.id}
+    );
+  });
+
+}
 
 module.exports.web =  web;
