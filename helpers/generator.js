da0b58af256d3dc46a556f7f9f61c5bd20c23fe9
Daniel Martinez <dmartz86@gmail.com>
2015-01-25 15:13:04 -0500

    Update route generator

--- helpers/generator.js
+++ helpers/generator.js
@@ -2,46 +2,30 @@
 var Zappy = require('../helpers/zappy').Zappy;
 var AV    = require('../config.json').APIVARS;
 // API
-var addRoutes = function(api, r, controller){
+var addRoutes = function(api, route, controller){
   var zappy = new Zappy(controller);
 
-  api.get( AV.PRE + r, function(req, res){
+  api.get( AV.PRE + route, function(req, res){
     zappy.Get(req, res);
   }); 
-  api.get( AV.PRE + r + AV.ID, function(req, res){
+  api.get( AV.PRE + route + AV.ID, function(req, res){
     zappy.GetOne(req, res);
   });
-  api.del( AV.PRE + r + AV.ID, function(req, res){
+  api.del( AV.PRE + route + AV.ID, function(req, res){
     zappy.Del(req, res);
   });
-  api.post(AV.PRE + r, function(req, res){
+  api.post(AV.PRE + route, function(req, res){
     zappy.Post(req, res);
   });
-  api.put( AV.PRE + r + AV.ID, function(req, res){
+  api.put( AV.PRE + route + AV.ID, function(req, res){
     zappy.Put(req, res);
   });
 };
 
 // WEB
-var INDEX_VIEW = 'index/index';
-
-var renderIndex = function(route, req, res){
-  res.render(INDEX_VIEW,
-    {model: route, id: req.params.id}
-  );
-};
-
 var addView =  function(web, route){
-  web.get('/' + route , function(req, res){
-    renderIndex(route, req, res);
-  });
-
-  web.get('/' + route + '/:id', function(req, res){
-    renderIndex(route, req, res);
-  });
-
-  web.get('/' + route + '/new', function(req, res){
-    renderIndex(route, req, res);
+  web.get(['/' + route, '/' + route + '/:id', '/' + route + '/new'] , function(req, res){
+    res.render('index/index',{model: route, id: req.params.id});
   });
 };
 
