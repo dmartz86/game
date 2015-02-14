1c9dcf59f50bb329dc602c097c1828dee9a93646
Daniel Martinez <dmartz86@gmail.com>
2015-02-14 16:28:25 -0500

    Updated message for register.

--- api/register.js
+++ api/register.js
@@ -3,17 +3,13 @@ var models = require('../helpers/models');
 var utils  = require('../helpers/utils');
 
 var addUser = function(email, cb){
-  console.log(email);
   models.users.FindOne({email: email}, function(err, user){
-    if(user || err){
-      console.log('addUser::FindOne ' + user + ' ' + err);
-      return cb(err || 'Email already registered.');
-    }
+    if(err){ return cb(err); }
+    if(user){ return cb(false); }
 
     var query = {email: email, code: utils.createUUID()};
     models.users.Insert(query, function(err, user ){
       if(err){
-        console.log('addUser::Insert ' + err);
         return cb('Error registering the email.');
       }
 
