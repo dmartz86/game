74fff364caa8c89e4d197001b43b52f305e8eb9f
Daniel Martinez <dmartz86@gmail.com>
2015-06-30 11:41:27 -0500

    Move test for mandril

--- examples/ping.mandril.js
+++
@@ -1,9 +0,0 @@
-var config = require('../config.json');
-var mandrill = require('mandrill-api/mandrill');
-
-var client = new mandrill.Mandrill(config.mandril.token);
-client.users.ping({key:config.mandril.token}, function(result) {
-  console.log(result);
-}, function(e) {
-  console.log(e);
-});
